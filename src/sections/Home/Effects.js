5e489dbd6ed0a5283396536b39b8e6c68227fcb5
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-03-15 11:57:41 +0530

    set animations and disable off-canvas rendering

--- src/sections/Home/Effects.js
+++ src/sections/Home/Effects.js
@@ -1,13 +1,14 @@
 import React, {Suspense} from 'react';
-import { EffectComposer, SSAO, ChromaticAberration, Bloom } from "react-postprocessing"
+import { EffectComposer, SSAO, ChromaticAberration, Bloom, Outline } from "react-postprocessing"
 
 function Effects(props) {
     return (
     <Suspense fallback={null}>
         <EffectComposer>
-            <ChromaticAberration offset={[0.00009, 0.00009]} />
+            {/* <ChromaticAberration offset={[0.00009, 0.00009]} /> */}
             <SSAO />
             <Bloom luminanceThreshold={0.6} />
+            <Outline />
         </EffectComposer>
     </Suspense>
     );
--- src/sections/Home/HeroSection.js
+++ src/sections/Home/HeroSection.js
@@ -1,39 +1,42 @@
-import React, { useRef } from "react"
-import { Canvas, useFrame } from "react-three-fiber"
+import React, { useRef, useState } from "react"
+import { Canvas, useFrame, useThree } from "react-three-fiber"
 import * as THREE from "three";
 import {
   softShadows,
   Text,
   PerspectiveCamera,
-  Cloud,
+  
 } from '@react-three/drei'
 import Effects from './Effects'
 import { useSpring, animated } from 'react-spring/three'
+import { useInView } from 'react-intersection-observer'
 
 
 softShadows();
 
 function HeroSection(props) {
-    
-    return (
-        <section>
-          <React.Suspense fallback={null}>
-            <Canvas shadowMap>
-              <PerspectiveCamera 
-                makeDefault
-                position={[0, 0, 8]}
-                rotation={[0.1,0,0]}
+    let [isActive,setActive] = useState(false)
+    let {pos} = useSpring({
+      pos: isActive ? [0,1,3] : [0,1.5,1],
+  
+    })
+    const { canvasRef, inView } = useInView()
 
-                />
+    return (
+        <section ref={canvasRef}>
+          <React.Suspense fallback={null}>
+            <Canvas  shadowMap>
+                {!inView && <DisableRender />}
+                <Camera />
                 {/* <fog attach="fog" args={["#272730", 3, 2]} /> */}
-                <React.Suspense fallback={null}>
+                {/* <React.Suspense fallback={null}>
                   <Cloud
-                    length={0.1}
-                    position={[-1, -5, 0]} 
-                    width={5} args={[3, 2]}
+                    length={0.07}
+                    position={[0, 0, 0]} 
+                    width={1} args={[3, 2]}
                     opacity={0.2}
                     />
-                </React.Suspense>
+                </React.Suspense> */}
                 <ambientLight intensity={0.2} />
                 <directionalLight
                   castShadow
@@ -62,7 +65,7 @@ function HeroSection(props) {
                   anchorX="center"
                   anchorY="middle"
                 >X</Text>
-                <Box />
+                <Box pos={pos} isActive={isActive} setActive={setActive}/>
                 <ShadowPlane />
                 <TEDxCarpet />
                 {/* <GroundPlane /> */}
@@ -75,16 +78,16 @@ function HeroSection(props) {
 
   export default HeroSection;
 
-  let Box = () =>{
+  let Box = ({isActive,setActive,pos}) =>{
     const box = useRef()
     useFrame(()=>{
       box.current.rotation.z = box.current.rotation.z += 0.01
       box.current.rotation.y = box.current.rotation.y += 0.01
     })
-    return <mesh ref={box} rotation={[90,0,0]} position={[0,1,0]} castShadow>
+    return <animated.mesh ref={box} onClick={e => setActive(!isActive)} rotation={[90,0,0]} position={pos} castShadow>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshPhysicalMaterial attach="material" color="white" />
-    </mesh>
+    </animated.mesh>
   }
 
 let ShadowPlane = () =>{
@@ -105,3 +108,24 @@ let TEDxCarpet = () =>{
       <meshPhongMaterial  attach='material' color="red" opacity={1} side={THREE.DoubleSide} />
     </mesh>
   }
+
+let Camera = () =>{
+  const { camera } = useThree();
+  const { z, rotZ } = useSpring({
+    from: { z: 0, rotZ: 0.5 },
+    config: { tension: 100, friction: 100 },
+    z: 8,
+    rotZ: 0
+  });
+  useFrame(() => {
+      camera.position.z = z.value;
+      camera.rotation.z = rotZ.value;
+  });
+  return <PerspectiveCamera 
+      makeDefault
+      position={[0,1,8]}
+      rotation={[0.1,0,0]}
+      />
+}
+
+const DisableRender = () => useFrame(() => null, 1000)
