b78a26fd0305f5e9fb74d1c0d41ec52492d81e88
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-03-31 15:12:59 +0530

    add helmet

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useEffect } from "react"
-export default function PageNotFound() {
+import Helmet from 'react-helmet'
+
+export default function ChatBot() {
 
     useEffect(()=>{
         window.location.replace("http://wa.me/918075217002?text=Hi");
